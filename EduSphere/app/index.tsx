7dc69579edd8c1b5232cd01007110431c91ed51f
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2024-11-07 00:40:42 +0530

    taiwind added and code cleaned

---
+++ EduSphere/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className='flex justify-center items-center w-full h-full bg-black'>
+      <Text className='text-red-500' >App</Text>
+    </View>
+  )
+}
+
+export default App
