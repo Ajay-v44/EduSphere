089cc21a0a5b33101ff996f5a265e569d998c15a
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2024-11-08 23:15:33 +0530

    authecticated using firebase auth

--- EduSphere/app/authentication/register.tsx
+++ EduSphere/app/authentication/register.tsx
@@ -1,26 +1,33 @@
 import { View, Text, TextInput, TouchableOpacity, ActivityIndicator, ToastAndroid } from 'react-native'
 import React, { useState } from 'react'
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { useRouter } from 'expo-router';
+import { onAuthStateChanged, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '@/initialize-firebase';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-const Login = () => {
+const Regiter = () => {
     const router = useRouter()
+    const [userName, setUserName] = useState<string | null>()
     const [email, setEmail] = useState<string | null>()
     const [password, setPassword] = useState<string | null>();
     const [loading, setLoading] = useState<boolean>(false)
-    const handleLogin = async () => {
+    const handleOnClick = async () => {
         try {
             setLoading(true)
             if (email && password !== null) {
-                const result = await signInWithEmailAndPassword(auth, email, password);
-                ToastAndroid.show('Logged In Successfully.', ToastAndroid.LONG)
+                const result = await createUserWithEmailAndPassword(auth, email, password);
+                await updateProfile(result.user, {
+                    displayName: userName,
+                });
+                ToastAndroid.show('Account Created Successfully.', ToastAndroid.LONG)
                 router.navigate("/index")
-            } else {
+            }
+            else {
                 ToastAndroid.show('Null Values Are Not Allowed. ', ToastAndroid.LONG)
             }
         } catch (err) {
+            // ToastAndroid.show('Oops Somwthing Went Wrong Try Again.. ', ToastAndroid.SHORT)
             alert(JSON.stringify(err?.code))
         } finally {
             setLoading(false)
@@ -29,10 +36,21 @@ const Login = () => {
     return (
         <View className='w-full h-full'>
             <View className='mt-20 m-5'>
-                <Text className='text-5xl font-bold'>Log In</Text>
-                <Text className='text-gray-400 font-light'>Enter Your Details to Login</Text>
+                <Text className='text-5xl font-bold'>Register User</Text>
+                <Text className='text-gray-400 font-light'>Enter Your Details to Register</Text>
             </View>
             <View className='m-5'>
+                <View className='m-5'>
+                    <View className='ml-5 '>
+                        <AntDesign name="user" className='relative top-20 right-10 h-20 w-10 text-center bg-red-300 pt-7' size={24} color="black" />
+                        <TextInput
+                            value={userName}
+                            onChangeText={setUserName}
+                            className='w-45 h-20 bg-violet-50'
+                            placeholder='Enter Your Name'
+                        />
+                    </View>
+                </View>
                 <View className='m-5'>
                     <View className='ml-5 '>
                         <MaterialCommunityIcons className=' relative top-20 right-10 h-20 w-10 text-center bg-red-300 pt-7' name="email-fast-outline" size={24} color="black" />
@@ -58,19 +76,22 @@ const Login = () => {
                     </View>
                 </View>
                 <View className='mt-10 flex items-center justify-center'>
-                    <TouchableOpacity onPress={handleLogin} className='bg-violet-400 w-72  h-20 rounded-md flex items-center justify-center'>
-                        <Text className=' text-white text-xl'> {loading ? <ActivityIndicator /> : 'Login'}</Text>
+                    <TouchableOpacity
+                        onPress={handleOnClick}
+                        className='bg-violet-400 w-72  h-20 rounded-md flex items-center justify-center' disabled={loading}>
+                        <Text className=' text-white text-xl'>{loading ? <ActivityIndicator /> : 'Register'}</Text>
                     </TouchableOpacity>
                 </View>
                 <View className='mt-3 flex flex-row justify-center'>
-                    <Text className='text-center pr-3'>Dont have account ?</Text>
+                    <Text className='text-center pr-3'>Already have an account ?</Text>
                     <TouchableOpacity onPress={() => {
-                        router.push("/authentication/register")
-                    }} ><Text className='underline'>Regiter</Text></TouchableOpacity>
+                        router.push("/authentication/login")
+                    }} ><Text className='underline'>Login</Text></TouchableOpacity>
                 </View>
             </View>
         </View>
+
     )
 }
 
-export default Login
+export default Regiter
--- EduSphere/app/index.tsx
+++ EduSphere/app/index.tsx
@@ -1,9 +1,24 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router'
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+
+import { auth } from '../initialize-firebase'
 
 const App = () => {
   const router = useRouter()
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      router.push("/authentication/login")
+    }
+  });
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <View className='flex justify-center items-center w-full h-full bg-black text-white'>
       <Text className='text-red-500' >App</Text>
@@ -11,6 +26,9 @@ const App = () => {
         router.push("/authentication/login")
       }}>
         <Text className='text-white' >Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={logout} className=' m-10 bg-red-400'>
+        <Text>Logout</Text>
       </TouchableOpacity>
     </View>
   )
