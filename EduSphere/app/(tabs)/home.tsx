5789922eaf0cd6ec4a17ef54b9e503db584b65ad
Ajay v <115262085+Ajay-v44@users.noreply.github.com>
2024-11-21 22:30:34 +0530

    github actions test

--- EduSphere/app/(tabs)/home.tsx
+++ EduSphere/app/(tabs)/home.tsx
@@ -1,35 +1,38 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
 import NavBar from '@/components/NavBar'
 import PostCard from '@/components/PostCard'
 
 
 const Home = () => {
-    const items={
+    const items = {
         id: 1,
         tittle: "testing",
         shortDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque asperiores cum dicta reiciendis iste sint voluptas ab fuga veritatis aperiam. Tempore provident nam quaerat sed itaque error ducimus vel ea!",
         imageUrl: "https://smiletutor.sg/wp-content/uploads/2020/12/AdobeStock_292618444-scaled.jpeg"
     }
     return (
-        <FlatList
-            className='w-full h-full bg-black'
-            data={[1]}
-            nestedScrollEnabled={true}
-            showsVerticalScrollIndicator={false}
-            renderItem={({ item }) => (
-                <View >
-                    <NavBar />
-                    <Text className="text-red-600 px-10 text-lg md:text-xl lg:text-2xl ">Posts</Text>
-                    <View className='m-5 md:m-10 lg:m-12 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 items-center '>
-                       {[1,2,3].map((item)=>{
-                        return  <PostCard  items={items} />
-                       })}
-                    </View>
-                </View>
-            )}
-        />
+        <View style={{ backgroundColor: 'black', height: "100px" }}>
+            <NavBar />
+            <Text className="text-red-600 px-10 text-lg md:text-xl lg:text-2xl ">Posts</Text>
+            <View style={{ flex: 1 ,flexDirection:"column",width:"100%",height:"100%"}}>
+               
+                    {/* {[1, 2, 3].map((item, index) => (
+                        <PostCard key={index} items={item} />
+                    ))} */}
+                    <PostCard items={items} />
+            </View>
+        </View>
     )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10, // Add some padding for better spacing
+        gap: 10,
+        width: "100%",
+        height: "100%"
+    },
+});
--- EduSphere/components/PostCard.tsx
+++ EduSphere/components/PostCard.tsx
@@ -11,7 +11,7 @@ interface PostCardData {
 const PostCard=({items}:PostCardData) => {
 
   return (
-    <View className='w-1/2 h-1/2  border-2 border-red-400 b rounded-full bg-white ' style={{backgroundColor:"white",width:"70%",height:"30%"}}>
+    <View className='w-1/2 h-1/2  border-2 border-red-400 b rounded-full bg-red-500 flex justify-center items-center' style={{backgroundColor:"red",width:"70%",height:"30%",borderRadius:50,margin:5}}>
       <Image source={{uri:`${items.imageUrl}`}} style={{width: 70, height: 80}} />
       <Text className='text-black'>{items.tittle}</Text>
     </View>
